a721c199a0f68a30bfadea7ff24180076da5f0f5
Ruthmy <ruthmy17@gmail.com>
2024-04-04 16:29:29 -0600

    Hello world with PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<h1>
+<?php
+echo "My first app";
+?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
