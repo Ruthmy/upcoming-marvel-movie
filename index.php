24b6680a2f3170c993b9e71d28b6087a9ded3fa5
Ruthmy <ruthmy17@gmail.com>
2024-04-05 20:25:39 -0600

    match statement

--- index.php
+++ index.php
@@ -3,32 +3,25 @@
     $isDev = true;
     $age = 21;
 
-    $isOld = $age > 30;
-
     var_dump($name);
     $output = "Erika ♥ " . "<br />" . "17/10/13" . "<br />" . $age . "<br />" . $isDev;
 
-    $outputAge = $isOld
-    ? "Eres viejo. Lo siento"
-    : "Eres joven. ¡Felicidades!";
+    $result = match (true) {
+        $age >= 65 => 'senior',
+        $age >= 25 => 'adult',
+        $age >= 18 => 'young adult',
+        default => 'kid',
+    };
+    
+    var_dump($result);
 
 ?>
 
+
+
 <h1>
-<?= $output .= "<br>" . $outputAge; ?>
+<?= $output; ?>
 </h1>
-
-<?php if ($isOld) : ?>
-    <h2>Old</h2>
-<?php elseif ($isDev) : ?>
-    <h2>Developer</h2>
-<?php else : ?>
-    <h2>Young</h2>
-<?php endif; ?>
-
-
-
-
 
 <style>
     :root {
