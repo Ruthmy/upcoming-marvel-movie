55c22e3b2d3cb01d231f372ed05f7bee2bc2a070
Ruthmy <ruthmy17@gmail.com>
2024-04-05 20:55:50 -0600

    asociative arrays

--- index.php
+++ index.php
@@ -5,17 +5,28 @@ $age = 21;
 
 $output = "Erika ♥ " . "<br />" . "17/10/13" . "<br />" . $age . "<br />" . $isDev;
 
-
-
+// arrays
 $bestLanguajes = ["PHP", "JavaScript", "Python", "Java", "C#"];
 $bestLanguajes[] = "C++";
 $bestLanguajes[4] = "Ruby";
+
+// asociative arrays
+$person = [
+    "llave" => "valor",
+    "name" => "Ruth",
+    "age" => 21,
+    "isDev" => true
+];
 
 ?>
 
 <ul>
     <?php foreach ($bestLanguajes as $key => $languaje) : ?>
         <li><?= $key . " - " . $languaje ?></li>
+    <?php endforeach; ?>
+        <hr>
+    <?php foreach ($person as $key => $value) : ?>
+        <li><?= $key . " - " . $value ?></li>
     <?php endforeach; ?>
 </ul>
 
