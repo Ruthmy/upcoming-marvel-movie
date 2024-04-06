a137695e95d288c2a020945e27a75f003388780d
Ruthmy <ruthmy17@gmail.com>
2024-04-05 19:23:17 -0600

    if new syntax

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
     $name = "Ruth";
     $isDev = true;
-    $age = (bool) 21;
+    $age = 21;
+
+    $isOld = $age > 30;
 
     var_dump($name);
     $output = "Erika ♥ " . "<br />" . "17/10/13" . "<br />" . $age . "<br />" . $isDev;
 ?>
 
-
 <h1>
 <?= $output; ?>
 </h1>
+
+<?php if ($isOld) : ?>
+    <h2>Old</h2>
+<?php elseif ($isDev) : ?>
+    <h2>Developer</h2>
+<?php else : ?>
+    <h2>Young</h2>
+<?php endif; ?>
+
+
+
+
 
 <style>
     :root {
